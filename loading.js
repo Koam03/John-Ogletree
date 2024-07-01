443543a25334801ca07b8f469cc6db90473b5dc8
Koam03 <131814728+Koam03@users.noreply.github.com>
2024-06-30 21:40:16 -0400

    about.html CNAME index.html loading-bar.js loading.css loading.html loading.js music-artwork.js script,js search_results.json service.html style.css images/graphics/06 20 24_JAOL.png images/graphics/Burst Through.png images/graphics/Canvas Backyard.jpeg images/graphics/Fallen Angel.jpg images/graphics/hero sections.jpg images/graphics/introverted cafe.png images/Music/Broken.jpg images/Music/Capture.jpg images/Music/Darkness.jpg images/Music/Deep End.jpg images/Music/Embers.jpg images/Music/Endless.jpg images/Music/Firepit.jpg images/Music/Flame.jpg images/Music/Game over.jpg images/Music/Glitchy.jpg images/Music/Land Of The Free.jpg images/Music/Lost.jpg images/Music/Midnight drive.jpg images/Music/Pink Moon.jpg images/Music/The end.jpg images/Music/The Horizon.jpg images/Music/Underwater.jpg images/Service Images/3D product animation.jpg images/Service Images/Album Cover Design.jpg images/Service Images/Audio ads production.jpg images/Service Images/Audio editing.jpg images/Service Images/Audiobook production.jpg images/Service Images/Beat making.jpg images/Service Images/Book Covers.jpg images/Service Images/Brand Style Guides.jpg images/Service Images/Brochure Design.jpg images/Service Images/Business Cards.jpg images/Service Images/Catalog Design.jpg images/Service Images/Custom songs.jpg images/Service Images/E-commerce Product Videos.jpg images/Service Images/Email Design.jpg images/Service Images/Flyer Design.jpg images/Service Images/Graphics for Streamers.jpg images/Service Images/Illustration.jpg images/Service Images/Infographic Design.jpg images/Service Images/Intro songs.jpg images/Service Images/Intro Videos.jpg images/Service Images/Invitation Design.jpg images/Service Images/Kid animations.jpg images/Service Images/Label Design.jpg images/Service Images/Logo Animation.jpg images/Service Images/Logo Design Service.jpg images/Service Images/Lottie animations.jpg images/Service Images/Menu Design.jpg images/Service Images/Music Videos.jpg images/Service Images/Outro songs.jpg images/Service Images/Outro Videos.jpg images/Service Images/Pattern Design.jpg images/Service Images/Podcast Cover Art.jpg images/Service Images/Podcast production.jpg images/Service Images/Poster Design.jpg images/Service Images/Presentation Design.jpg images/Service Images/Product Videos.jpg images/Service Images/Resume Design.jpg images/Service Images/Signage Design.jpg images/Service Images/Slideshow Videos.jpg images/Service Images/Social Media Kit Design.jpg images/Service Images/Social Media Videos.jpg images/Service Images/Streamer animations.jpg images/Service Images/Subtitles-Captions.jpg images/Service Images/Tattoo Design.jpg images/Service Images/Text animations.jpg images/Service Images/Vector Tracing.jpg images/Service Images/VFX.jpg images/Service Images/Video Ads.jpg images/Service Images/Video Editing.jpg images/Service Images/Web animations.jpg images/Service Images/Web Banners.jpg images/Service Images/Website Design.jpg images/social links/dribbble-logo-bold-svgrepo-com.svg images/social links/Facebook Link.svg images/social links/github-svgrepo-com.svg images/social links/instagram-round-svgrepo-com.svg images/social links/instagram-svgrepo-com.svg images/social links/social-twitter-svgrepo-com.svg images/social links/spotify-with-circle-svgrepo-com.svg images/social links/youtube-round-svgrepo-com.svg

--- loading.js
+++
@@ -1,7 +0,0 @@
-// (Optionally) You can use this to update the loading message
-const loadingMessage = document.getElementById('loading-message');
-
-// Example: Change message after a delay
-setTimeout(() => {
-  loadingMessage.textContent = "Almost there...";
-}, 3000); // 3 seconds
