6ba9e9da6170c22b11668c9eb7ff39036bff8365
John Ogletree <131814728+Koam03@users.noreply.github.com>
2024-09-16 13:55:45 -0400

    index.html assets/scripts/navtoggle.js assets/stylesheets/index.css

--- assets/scripts/navtoggle.js
+++ assets/scripts/navtoggle.js
@@ -2,13 +2,12 @@ document.getElementById('sidebar-toggle').addEventListener('click', function () 
     var sidebar = document.getElementById('sidebar');
     var content = document.querySelector('.content');
 
-    if (sidebar.style.display === 'none' || !sidebar.style.display) {
-        sidebar.style.display = 'block';
+    // Toggle the 'hide-nav' class to show/hide navigation links
+    if (sidebar.classList.contains('hide-nav')) {
+        sidebar.classList.remove('hide-nav');
         content.style.marginLeft = '250px';
-        content.style.width = 'calc(100% - 250px)';
     } else {
-        sidebar.style.display = 'none';
-        content.style.marginLeft = '0';
-        content.style.width = '100%';
+        sidebar.classList.add('hide-nav');
+        content.style.marginLeft = '50px'; // Adjust content margin to match new sidebar width
     }
 });
