a45f6a4449f9db3aacf3ac4b9b713934debc2de3
John Ogletree <131814728+Koam03@users.noreply.github.com>
2024-09-16 09:40:00 -0400

    401.html 404.html detail_author.html detail_book-series.html detail_product.html detail_project-category.html detail_project.html detail_search-results.html detail_service.html detail_services-catagory.html detail_sitemap-pages.html detail_social-icons.html detail_song.html index.html search.html assets/scripts/google-analytics.js assets/scripts/navtoggle.js assets/stylesheets/index.css css/john-361fb7.webflow.css css/normalize.css css/webflow.css fonts/SchibstedGrotesk-Italicwght.woff2 fonts/SchibstedGroteskwght.woff2 images/android-chrome-256x256.png images/favicon.png images/webclip.png js/webflow.js

--- assets/scripts/navtoggle.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("mobile-nav-toggle").addEventListener("click", function() {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("active");
-});
-
-document.getElementById("sidebar-close").addEventListener("click", function() {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("active");
-});
