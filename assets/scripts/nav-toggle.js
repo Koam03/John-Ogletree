19ed994baabf057928ed8adf47829669cd83300f
John Ogletree <131814728+Koam03@users.noreply.github.com>
2024-09-17 16:08:29 -0400

    index.html assets/scripts/nav-toggle.js assets/stylesheets/index.css

---
+++ assets/scripts/nav-toggle.js
@@ -0,0 +1,7 @@
+// Sidebar toggle functionality
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('sidebar-visible');
+});
