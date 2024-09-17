088acb508a66c84d035822f514f618ca0b6caffe
John Ogletree <131814728+Koam03@users.noreply.github.com>
2024-09-17 15:52:59 -0400

    index.html assets/css/index.css assets/js/nav-toggle.js

--- assets/js/nav-toggle.js
+++
@@ -1,8 +0,0 @@
-// Toggle for sidebar visibility
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.getElementById('sidebar');
-
-// Add click event listener for the menu toggle button
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('sidebar-visible');
-});
