82f7ec29584f0088e06de8225d835c1dcdf0e345
John Ogletree <131814728+Koam03@users.noreply.github.com>
2024-09-17 15:07:45 -0400

    index.html assets/css/index.css assets/js/nav-toggle.js

--- assets/js/nav-toggle.js
+++ assets/js/nav-toggle.js
@@ -1,28 +1,8 @@
-// Function to check screen width and initialize sidebar visibility
-function checkScreenWidth() {
-    const sidebar = document.getElementById('sidebar');
-    const toggleButton = document.getElementById('menu-toggle');
-  
-    if (window.innerWidth <= 768) {
-      // For smaller screens, hide the sidebar and show the menu toggle button
-      sidebar.classList.add('sidebar-hidden');
-      toggleButton.style.display = 'flex';
-    } else {
-      // For larger screens, show the sidebar and hide the menu toggle button
-      sidebar.classList.remove('sidebar-hidden');
-      toggleButton.style.display = 'none';
-    }
-  }
-  
-  // Toggle sidebar visibility on smaller screens
-  document.getElementById('menu-toggle').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
+// Toggle for sidebar visibility
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+
+// Add click event listener for the menu toggle button
+menuToggle.addEventListener('click', () => {
     sidebar.classList.toggle('sidebar-visible');
-    sidebar.classList.toggle('sidebar-hidden');
-  });
-  
-  // Run the checkScreenWidth function on initial load
-  checkScreenWidth();
-  
-  // Add event listener for window resize to handle responsive changes
-  window.addEventListener('resize', checkScreenWidth);  
+});
